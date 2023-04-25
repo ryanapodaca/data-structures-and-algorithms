99aaaeb50b616751f8e70981a31e895eaf98c37c
Ryan Apodaca <rapodaca1@my.hpu.edu>
2023-04-24 18:08:04 -1000

    cc5

--- java/datastructures/lib/src/main/java/datastructures/linkedlist/LinkedList.java
+++ java/datastructures/lib/src/main/java/datastructures/linkedlist/LinkedList.java
@@ -1,10 +1,45 @@
 package datastructures.linkedlist;
 
+import datastructures.linkedlist.Node;
+
 public class LinkedList
 {
-  @Override
-  public String toString()
-    {
-        return "null";  // TODO: implement me
+  public Node head = null;
+  Node tail = null;
+
+  public void insert(int value) {
+    Node newNode = new Node(value);
+    newNode.setNext(head);
+    head = newNode;
+  }
+  public boolean includes(int value) {
+    Node current = head;
+    while (current != null){
+      if (current.getValue() == value) {
+        return true;
+      }
+      current = current.getNext();
     }
+    return false;
+  }
+  @Override
+  public String toString() {
+    if (head == null)
+      return null;
+
+    StringBuilder sb = new StringBuilder();
+    Node current = head;
+    while (current != null) {
+      sb.append("{ ");
+      sb.append(current.getValue());
+      sb.append(" } -> ");
+      current = current.getNext();
+    }
+    sb.append("NULL");
+    return sb.toString();
+    }
+
+  public Node getHead() {
+    return this.head;
+  }
 }
--- java/datastructures/lib/src/test/java/datastructures/linkedlist/LinkedListTest.java
+++ java/datastructures/lib/src/test/java/datastructures/linkedlist/LinkedListTest.java
@@ -2,14 +2,63 @@ package datastructures.linkedlist;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
-public class LinkedListTest
-{
-  @Test void testAMethod()
-    {
-      LinkedList sut = new LinkedList();
-      System.out.println(sut);
-      assertTrue(true);
+
+public class LinkedListTest {
+  @Test void testInstantiateEmptyLL() {
+      LinkedList list = new LinkedList();
+      assertNull(list.toString());
     }
+
+  @Test void insertTest(){
+    LinkedList list = new LinkedList();
+    list.insert(5);
+    assertTrue(list.includes(5));
+
+  }
+
+  @Test void testHead() {
+    LinkedList list = new LinkedList();
+    list.insert(1);
+    list.insert(2);
+    list.insert(3);
+    assertEquals(3,list.getHead().getValue());
+  }
+
+  @Test public void testInsertMultipleNodes() {
+    LinkedList list = new LinkedList();
+    list.insert(1);
+    list.insert(2);
+    list.insert(3);
+    assertEquals(3, list.getHead().getValue());
+    assertEquals(2, list.getHead().getNext().getValue());
+    assertEquals(1, list.getHead().getNext().getNext().getValue());
+  }
+
+  @Test public void testFindExistingValue() {
+    LinkedList list = new LinkedList();
+    list.insert(1);
+    list.insert(2);
+    list.insert(3);
+    assertTrue(list.includes(2));
+  }
+
+  @Test public void testFindNonExistingValue() {
+    LinkedList list = new LinkedList();
+    list.insert(1);
+    list.insert(2);
+    list.insert(3);
+    assertFalse(list.includes(4));
+  }
+
+  @Test public void testReturnAllValues() {
+    LinkedList list = new LinkedList();
+    list.insert(1);
+    list.insert(2);
+    list.insert(3);
+    assertEquals("{ 3 } -> { 2 } -> { 1 } -> NULL", list.toString());
+  }
+
+
 }
