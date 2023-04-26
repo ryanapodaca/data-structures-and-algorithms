2d8fab81a3ac21b456dc7abaff6298c5a46210fe
Ryan Apodaca <rapodaca1@my.hpu.edu>
2023-04-26 13:38:47 -1000

    getting there

--- java/datastructures/lib/src/test/java/codechallenges/linkedlist/LinkedListChallengesTest.java
+++ java/datastructures/lib/src/test/java/codechallenges/linkedlist/LinkedListChallengesTest.java
@@ -1,17 +1,35 @@
 package codechallenges.linkedlist;
 
+import datastructures.linkedlist.*;
+
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class LinkedListChallengesTest
 {
-  @Test
-  void testAChallenge()
-  {
-    LinkedListChallenges sut = new LinkedListChallenges();
-    sut.linkedListChallenge06();
-    System.out.println("test something here");
-    assertTrue(true);
+
+
+
+  @Test void testZipFunction() {
+    LinkedList ll1 = new LinkedList();
+    ll1.insert(1);
+    ll1.insert(3);
+    ll1.insert(5);
+
+    LinkedList ll2 = new LinkedList();
+    ll1.insert(2);
+    ll1.insert(4);
+    ll1.insert(6);
+
+    LinkedList test = new LinkedList();
+    ll1.insert(1);
+    ll1.insert(2);
+    ll1.insert(3);
+    ll1.insert(4);
+    ll1.insert(5);
+    ll1.insert(6);
+    assertEquals(test, zipLinkedLists(ll1,ll2));
   }
 }
